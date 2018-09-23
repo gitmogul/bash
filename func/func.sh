e7beaa57439179284c9a6623922fc7139c93906c
Shenoy Gurudutt <shenoyg@drona.localdomain>
2018-09-23 16:20:24 -0700

    deleted garbage files

--- func/func.sh
+++ func/func.sh
@@ -3,14 +3,16 @@
 ###############################################################################
 function pass_by_reference()
 {
-    local return_value1=$1
-    local return_value2=$2
+    # note after the assignment val1=value1
+    # make sure local var name and global name are not the same!
+    local val1=$1
+    local val2=$2
 
     local num1=10
     local num2=20
 
-    eval $return_value1="'$num1'"
-    eval $return_value2="'$num2'"
+    eval $val1="$num1"
+    eval $val2="$num2"
 }
 
 ###############################################################################
@@ -20,11 +22,11 @@ function pass_by_value()
     echo "$result"
 }
 
-val1=1
-val2=2
-echo "Before pass_by_reference values: $val1, $val2"
-pass_by_reference val1 val2
-echo "After pass_by_reference values: $val1, $val2"
+value1=1
+value2=2
+echo "Before pass_by_reference values: $value1, $value2"
+pass_by_reference value1 value2
+echo "After pass_by_reference values: $value1, $value2"
 
 val1=1
 val2=2
